0f551afd5c51fd851bc2823eb3a9f5fcbb7ebd0c
abos-gergo <abos.gergo@students.jedlik.eu>
2021-09-11 22:04:21 +0200

    Generálás működik, megjeleníti a négyzetet, elhelyezés és a különböző képek megjelenítése hiányzik

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from mapgen import *
+import pygame
+from pygame import *
+
+WIN = pygame.display.set_mode((1920, 900))
+WIN.fill((255, 255, 255))
+
 def main() -> None:
-    pass
+    map : Map = Map(18, 100)
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
 
 if __name__ == "__main__":
--- mapgen.py
+++ mapgen.py
@@ -1,13 +1,79 @@
 from typing import List
 import engine
 import random
+from assets import Sprites
+import main
+import pygame
 
 class Tile:
     def __init__(self, x:int, y:int, z:int) -> None:
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x : int = x
+        self.y : int = y
+        self.z : int = z
+        self.neighbors : List[tuple] = []
 
+    def getNeighbors(self, map):
+        neighborspos : List[tuple] = []
+        neighbors : List[bool] = [0, 0, 0, 0]
+        for neighbor in Map.tiles:
+            if engine.Distance(self, neighbor) <= 1:
+                if neighbor.y < self.y: # NE
+                    neighbors[0] = 1
+                elif neighbor.x > self.x: #SE
+                    neighbors[1] = 1
+                elif neighbor.y > self.y: #SW
+                    neighbors[2] = 1
+                elif neighbor.x < self.x: #NW
+                    neighbors[3] = 1
+        for i, v in enumerate(neighbors):
+            if i == 0 and v == 0:
+                pos = (self.x, self.y - 1)
+                neighborspos.append(pos)
+            if i == 1 and v == 0:
+                pos = (self.x + 1, self.y)
+                neighborspos.append(pos)
+            if i == 2 and v == 0:
+                pos = (self.x, self.y + 1)
+                neighborspos.append(pos)
+            if i == 3 and v == 0:
+                pos = (self.x  - 1, self.y)
+                neighborspos.append(pos)
+        self.neighbors = neighborspos
+
+    def getTileType(self) -> str:
+        for tile in Map.tiles:
+            string : str = ''
+            spaces = 0
+            for space in self.neighbors:
+                if space == False:
+                    spaces += 1
+            if spaces == 1:
+                pass
+
+            elif spaces == 2:
+                if tile.neighbors[0] == 0 and tile.neighbors[1] == 0:
+                    string = 'W'
+                elif tile.neighbors[0] == 0 and tile.neighbors[3] == 0:
+                    string = 'S'
+                elif tile.neighbors[2] == 0 and tile.neighbors[3] == 0:
+                    string = 'E'
+                elif tile.neighbors[2] == 0 and tile.neighbors[1] == 0:
+                    string = 'N'
+
+            elif spaces == 3:
+                if tile.neighbors[0]  == 0 and tile.neighbors[1]  == 0 and tile.neighbors[2] == 0:
+                    string = 'NW'
+                elif tile.neighbors[1]  == 0 and tile.neighbors[2]  == 0 and tile.neighbors[3] == 0:
+                    string = 'NE'
+                elif tile.neighbors[2]  == 0 and tile.neighbors[3]  == 0 and tile.neighbors[0] == 0:
+                    string = 'SE'
+                elif tile.neighbors[3]  == 0 and tile.neighbors[0]  == 0 and tile.neighbors[1] == 0:
+                    string = 'SW'
+
+            elif spaces == 4:
+                string = 'M'
+
+        return string
 
 class Map:
     tiles : List[Tile] = []
@@ -17,45 +83,69 @@ class Map:
     def __init__(self, maxsize : int, tilecount: int) -> None:
         self.maxsize = maxsize
         self.tilecount = tilecount
+        Map.generateTiles(self)
 
-    def generateTiles(map):
-        tile = Tile(0, 0, 0)
-        map.tiles.append(tile)
+    def generateTiles(self):
+        Map.tiles.append(Tile(0 + self.maxsize / 2, 0 + self.maxsize / 2, 0))
         freespaces : List[tuple] = []
-        placedtiles : int = 0
-        while placedtiles != map.tilecount - 1:
-            # Get neighbours ---------------------------------
-            for tile in map.tiles:
-                fr : List[bool] = [0, 0, 0, 0]
-                for neighbor in map.tiles:
-                    if engine.Distance(tile, neighbor) == 1:
-                        if neighbor.x < 0 or neighbor.x > map.maxsize / 2 or neighbor.y < 0 or neighbor.y > map.maxsize / 2:
-                            if neighbor.y < tile.y: # NE
-                                fr[0] = 1
-                            elif neighbor.x > tile.x: #SE
-                                fr[1] = 1
-                            elif neighbor.y > tile.y: #SW
-                                fr[2] = 1
-                            elif neighbor.x < tile.x: #NW
-                                fr[3] = 1
-                for i, v in enumerate(fr):
-                    if i == 0 and v == 0:
-                        pos = (tile.x, tile.y - 1)
-                        freespaces.append(pos)
-                    elif i == 1 and v == 0:
-                        pos = (tile.x + 1, tile.y)
-                        freespaces.append(pos)
-                    elif i == 2 and v == 0:
-                        pos = (tile.x, tile.y + 1)
-                        freespaces.append(pos)
-                    elif i == 3 and v == 0:
-                        pos = (tile.x  - 1, tile.y)
-                        freespaces.append(pos)
+        placedtiles : int = 1
+        while placedtiles != self.tilecount:
+            freespaces.clear()
+            for tile in Map.tiles:
+                tile.getNeighbors(self)
+                for freespace in tile.neighbors:
+                    freespaces.append(freespace)
             freespace = freespaces[random.randint(0, freespaces.__len__() - 1)]
-            map.tiles.append(Tile(freespace[0], freespace[1], 0))
+            if freespace[0] <= 0 or freespace[0] > self.maxsize or freespace[1] <= 0 or freespace[1] > self.maxsize:
+                continue
+            Map.tiles.append(Tile(freespace[0], freespace[1], 0))
             placedtiles += 1
+        temptiles : List[Tile] = list(Map.tiles)
+        row : List[Tile] = []
+        lowx = temptiles[0].x
+        lowy = temptiles[0].y
+        highx = temptiles[0].x
+        highy = temptiles[0].y
+        for tile in temptiles:
+            if tile.x < lowx:
+                    lowx = tile.x
+            elif tile.x > highx:
+                highx = tile.x
+            if tile.y < lowy:
+                lowy = tile.y
+            elif tile.y > highy:
+                highy = tile.y
+        
+        while temptiles.__len__() != 0:
+            row.clear()
+            lowx = temptiles[0].x
+            lowy = temptiles[0].y
+            highx = temptiles[0].x
+            highy = temptiles[0].y
 
+            for tile in temptiles:
+                if tile.x < lowx:
+                    lowx = tile.x
+                elif tile.x > highx:
+                    highx = tile.x
+            origin = ()
+            for tile in temptiles:
+                if tile.x == lowx:
+                    row.append(tile)
 
+            lowy : int = row[0].y
+            for tile in row:
+                if tile.y < lowy:
+                    lowy = tile.y
+                elif tile.y > highy:
+                    highy = tile.y
 
-map : Map = Map(10, 1)
-map.generateTiles()
+            for tile in row:
+                if tile.y == lowy:
+                    pos : tuple = (944 + (tile.x - 1) * 32 - (tile.y - 1) * 32, 529 + (tile.x - 1) * 16 + (tile.y - 1) * 16)
+                    print(f'Image at: {(tile.x, tile.y)}')
+                    main.WIN.blit(pygame.image.load('Assets/Map/Test.png'), pos)
+                    pygame.display.update()
+                    row.remove(tile)
+                    temptiles.remove(tile)
+            
