6d1c59b6ff7f191b0c506f3dc7e76f4738ce662a
pappadam <papp.adam@students.jedlik.eu>
2021-09-12 12:33:51 +0200

    Mouse click detection works with tiles
    The function returns to a list, which contains the coordinates of the chosen tile according to the monitor

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+import m_click_det
 from mapgen import *
 import pygame
+from m_click_det import *
 from pygame import *
 
 WIN = pygame.display.set_mode((1920, 900))
 WIN.fill((255, 255, 255))
 
 def main() -> None:
-    map : Map = Map(18, 100)
+    map : Map = Map(10, 100)
 
     run = True
     while run:
+        m_click_det.mouse_click.tile_detection(mouse_click())
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
