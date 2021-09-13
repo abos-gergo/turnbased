7b321c8a1e834742af7cd97d3e97cf8a12843089
abos-gergo <abos.gergo@students.jedlik.eu>
2021-09-13 12:05:34 +0200

    Solved pylance issues in engine.py

--- engine.py
+++ engine.py
@@ -1,19 +1,17 @@
-import math
-from typing import List
-import numpy as np
+from typing import Any, List
 import pygame
 
 
-def Distance(tile1, tile2) -> int:
+def Distance(tile1: Any, tile2: Any) -> int:
     distx = abs(tile1.x - tile2.x)
     disty = abs(tile1.y - tile2.y)
     return distx + disty
 
 
 def MoveTowards(
-    start: tuple, target: tuple, speed: float
-) -> tuple:  # Returns x and y value, both are whole numbers
-    dest: List = []
+    start: tuple[int, int], target: tuple[int, int], speed: float
+) -> tuple[int, int]:  # Returns x and y value, both are whole numbers
+    dest: List[int] = []
     if start != target:
         startvector = pygame.Vector2(start)
         targetvector = pygame.Vector2(target)
@@ -24,5 +22,6 @@ def MoveTowards(
         if dest[1] > target[1]:
             dest[1] = target[1]
     else:
-        dest = target
-    return tuple(dest)
+        dest = list(target)
+    returned: tuple[int, int] = (dest[0], dest[1])
+    return returned
