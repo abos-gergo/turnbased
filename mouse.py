e64beb7965f9e01dc1f67e2f21f5d89c6f8d669c
pappadam <papp.adam@students.jedlik.eu>
2021-09-14 15:38:39 +0200

    added mouse_pos function

--- mouse.py
+++ mouse.py
@@ -1,11 +1,14 @@
-from _typeshed import Self
+import pygame.mouse
+
 import mapgen
 import engine
 
 class click:
     def __init__(self) -> None:
         self.button: int
-        self.mousepos: tuple[int, int]
+
+    def pos(self) -> tuple[int, int]:
+        return pygame.mouse.get_pos()
 
     def clickedOnTile(self):
         lowestDist = engine.pixelDistance(mapgen.Map.tiles[0], self.mousepos)
